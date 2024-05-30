10231f843fcf1bea8b3691c756004964ca0ddf47
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-05-30 03:03:42 +0100

    "Added PlaidLink component to MyBanks page, modified CSS classes in globals.css, and updated ConnectCard component to be hidden on small screens."

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,5 +1,6 @@
 import BankCard from '@/components/BankCard';
 import ConnectCard from '@/components/ConnectCard';
+import PlaidLink from '@/components/PlaidLink';
 import { getAccounts } from '@/lib/actions/bank.action';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
@@ -17,11 +18,17 @@ const MyBanks = async () => {
     return (
         <div className='flex rounded-tl-3xl bg-white'>
             <div className="my-banks">
-                <div className="space-y-4">
-                    <h2 className="header-2">
-                        Your cards
-                    </h2>
-                    <div className="flex flex-wrap gap-6">
+                <div className="space-y-10 md:space-y-5 w-fit">
+                    <div className='flex items-center justify-between'>
+                        <h2 className="header-2">
+                            Your cards
+                        </h2>
+
+                        <span className='lg:hidden'>
+                            <PlaidLink user={loggedIn} variant='primary' />
+                        </span>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pb-20 place-content-center">
                         {accounts && accounts.data.map((a: Account) => (
                             <BankCard
                                 key={accounts.id}
--- components/ConnectCard.tsx
+++ components/ConnectCard.tsx
@@ -45,7 +45,7 @@ const ConnectCard = ({ user }: ConnetCardProps) => {
     return (
         <button
             onClick={() => open()}
-            className="relative flex flex-col gap-2 h-[190px] w-full max-w-[320px] justify-center text-blue-600 border-blue-400 items-center rounded-[20px] border hover:border-blue-600 hover:scale-95 animate-in transition-all duration-300 ease-in-out shadow-creditCard backdrop-blur-[6px]"
+            className="relative hidden lg:flex flex-col gap-2 h-[190px] w-full max-w-[320px] justify-center text-blue-600 border-blue-400 items-center rounded-[20px] border hover:border-blue-600 hover:scale-95 animate-in transition-all duration-300 ease-in-out shadow-creditCard backdrop-blur-[6px]"
         >
 
             <Plus />
